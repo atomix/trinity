7dc5f43499df26cfe63bde25502c305b5f295d34
Jonathan Halterman <jhalterman@gmail.com>
2015-10-28 17:26:32 -0700

    Update push doc path

--- bin/push-docs.sh
+++ bin/push-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # run from top level dir
 rm -rf target/docs
-git clone git@github.com:jhalterman/figaro.git target/docs -b gh-pages
+git clone git@github.com:atomix/trinity.git target/docs -b gh-pages
 lein doc
 cd target/docs
 git add -A
