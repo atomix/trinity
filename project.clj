7b1f63fc840b394571b0306fcfbcbee9a01ee51d
Jon Hall <jhall@ciena.com>
2017-02-05 20:31:58 -0800

    Use release versions instead of SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject io.atomix/trinity "1.0.0-SNAPSHOT"
+(defproject io.atomix/trinity "1.0.0"
   :description "A sweet little Clojure API for Atomix"
   :url "http://github.com/atomix/trinity"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.atomix/atomix-all "1.0.1-SNAPSHOT"]
+                 [io.atomix/atomix-all "1.0.0"]
                  [io.atomix.catalyst/catalyst-netty "1.1.2"]]
   :repositories [["sonatype-nexus-snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
   :plugins [[lein-codox "0.9.0"]
